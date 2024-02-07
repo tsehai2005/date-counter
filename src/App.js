28810bf1f5c4d6b1c907793b1bb73ba96f23c5fd
Tsegay <tsegayhailemariam444@gmail.com>
2024-02-07 14:00:31 +0300

    date counter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { useState } from "react";
+import "./style.css";
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter />
     </div>
   );
 }
+function Counter() {
+  const [step, setStep] = useState(0);
+  const [count, setCount] = useState(0);
+  const date = new Date(2027, 6, 21);
+  date.setDate(date.getDate() + count);
+  const formatedDate = date.toDateString();
 
-export default App;
+  function decrementStep() {
+    if (step > 0) setStep((s) => s - 1);
+  }
+  function incrementStep() {
+    setStep((s) => s + 1);
+  }
+  function decrementCount() {
+    if (count > 0) setCount((c) => (c - step > 0 ? c - step : 0));
+  }
+  function incrementCount() {
+    setCount((c) => c + step);
+  }
+  return (
+    <div>
+      <div>
+        <button onClick={decrementStep}>-</button>
+        <span> Step: {step}</span>
+        <button onClick={incrementStep}>+</button>
+      </div>
+      <div>
+        <button onClick={decrementCount}>-</button>
+        <span>Count: {count}</span>
+        <button onClick={incrementCount}>+</button>
+      </div>
+      <div>
+        {count === 0 ? (
+          <p>Today is {formatedDate}</p>
+        ) : (
+          <p>
+            {count} days from today is {formatedDate}
+          </p>
+        )}
+      </div>
+    </div>
+  );
+}
